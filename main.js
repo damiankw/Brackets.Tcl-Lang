c7e431ef5e8be3ebf3b99027cfadfd6d929dc933
Damian West <damian@damian.id.au>
2017-10-22 12:50:10 +1100

    Added Brackets configuration.

---
+++ main.js
@@ -0,0 +1,16 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets, window */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    var LanguageManager = brackets.getModule("language/LanguageManager");
+
+    LanguageManager.defineLanguage("swift", {
+        name: "Tcl",
+        mode: "tcl",
+        fileExtensions: ["tcl", "etcl"],
+        blockComment: ["/*", "*/"],
+        lineComment: ["#"]
+    });
+}); 
